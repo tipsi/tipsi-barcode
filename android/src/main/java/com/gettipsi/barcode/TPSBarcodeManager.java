bd97320f336735de821b961bf3c28432624bddbc
Anton Kuznetsov <isnifer@gmail.com>
2016-12-14 15:03:37 +0300

    Remove redundant mReaderView.stop();

--- android/src/main/java/com/gettipsi/barcode/TPSBarcodeManager.java
+++ android/src/main/java/com/gettipsi/barcode/TPSBarcodeManager.java
@@ -44,7 +44,6 @@ public class TPSBarcodeManager extends ViewGroupManager<TPSBarcodeView> implemen
 
     @Override
     public void onHostDestroy() {
-        mReaderView.stop();
         mReaderView.release();
     }
 }
