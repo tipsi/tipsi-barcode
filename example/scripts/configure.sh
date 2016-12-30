08b8893a929c945997e0369dffce5dbdd41d745f
Anton Kuznetsov <isnifer@gmail.com>
2016-12-31 02:04:15 +0300

    Update configure.sh

--- example/scripts/configure.sh
+++ example/scripts/configure.sh
@@ -3,12 +3,15 @@ isMacOS() {
 }
 
 if isMacOS; then
-  # Add location access description key/value to info.plist
-  /usr/libexec/PlistBuddy -c "Add :NSLocationWhenInUseUsageDescription string 'Access to your current location information (GPS) is needed to find the nearest points of interest around your current position.'" "${DESTINATION_DIRECTORY}"/../platforms/ios/"${PROJECT_NAME}"/"${PROJECT_NAME}"-Info.plist
+  # Add microphone access description key/value to info.plist
+  /usr/libexec/PlistBuddy -c "Add :NSMicrophoneUsageDescription string '\$(PRODUCT_NAME) microphone use'" ./ios/example/Info.plist
 
   # Add camera access description key/value to info.plist
-  /usr/libexec/PlistBuddy -c "Add :NSCameraUsageDescription string 'Access to the camera is needed to display augmented reality content on top of your camera image.'" "${DESTINATION_DIRECTORY}"/../platforms/ios/"${PROJECT_NAME}"/"${PROJECT_NAME}"-Info.plist
+  /usr/libexec/PlistBuddy -c "Add :NSCameraUsageDescription string '\$(PRODUCT_NAME) camera use'" ./ios/example/Info.plist
 
   # Add photo library access description key/value to info.plist
-  /usr/libexec/PlistBuddy -c "Add :NSPhotoLibraryUsageDescription string 'Access to your photo library is required for the example 'Bonus: Capture Screen' because it adds a screenshot of the current Architect view.'" "${DESTINATION_DIRECTORY}"/../platforms/ios/"${PROJECT_NAME}"/"${PROJECT_NAME}"-Info.plist
+  /usr/libexec/PlistBuddy -c "Add :NSPhotoLibraryUsageDescription string '\$(PRODUCT_NAME) photo use'" ./ios/example/Info.plist
+
+  # Add media access description key/value to info.plist
+  /usr/libexec/PlistBuddy -c "Add :NSAppleMusicUsageDescription string '\$(PRODUCT_NAME) media use'" ./ios/example/Info.plist
 fi
